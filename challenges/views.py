872d989dd3b6f2f67b1b96c7c8050392fe25dc78
Abishek Balaji <abishekbalaji7.151@gmail.com>
2022-12-18 19:16:29 +0530

    Added dynamic html

--- challenges/views.py
+++ challenges/views.py
@@ -15,21 +15,25 @@ monthly_challenges = {
     'september': 'Learn django for atleats 20 mins everyday',
     'october': 'Eat no meat for the month',
     'november': 'Walk atleast for 20 mins everyday',
-    'december': 'Learn django for atleats 20 mins everyday'
+    'december': None
 }
 
 # Create your views here.
 
 
 def index(request):
-    list_items = ""
+    # list_items = ""
 
-    for month in monthly_challenges:
-        month_path = reverse('month-challenge', args=[month])
-        list_items += f'<li><a href="{month_path}">{month.capitalize()}</a></li>'
+    return render(request, 'challenges/index.html', {
+        'months': [month for month in monthly_challenges]
+    })
 
-    response_data = f'<ul>{list_items}</ul>'
-    return HttpResponse(response_data)
+    # for month in monthly_challenges:
+    #     month_path = reverse('month-challenge', args=[month])
+    #     list_items += f'<li><a href="{month_path}">{month.capitalize()}</a></li>'
+
+    # response_data = f'<ul>{list_items}</ul>'
+    # return HttpResponse(response_data)
 
 
 def monthly_challenge_by_no(request, month):
@@ -41,10 +45,11 @@ def monthly_challenge_by_no(request, month):
 
 
 def monthly_challenge(request, month):
-    print("*********request: ", request,
-          dir(request), request.body, request.path)
-    challenge_text = monthly_challenges.get(month, None)
-    response_data = f'<h1>{challenge_text}</h1>'
-    if challenge_text:
-        return HttpResponse(response_data)
-    return HttpResponseNotFound(f'<h1>This month {month} is not supported.</h1>')
+    try:
+        challenge_text = monthly_challenges[month]
+        return render(request, 'challenges/challenge.html', {
+            'month': month,
+            'text': challenge_text
+        })
+    except:
+        return HttpResponseNotFound(f'<h1>This month {month} is not supported.</h1>')
